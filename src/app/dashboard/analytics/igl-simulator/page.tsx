9693afc19ceb176f124c81776c5305816aca7552
Hunter Galloway <huntergallowaydev@gmail.com>
2025-05-08 01:40:28 -0400

    State fix

--- src/app/dashboard/analytics/igl-simulator/page.tsx
+++ src/app/dashboard/analytics/igl-simulator/page.tsx
@@ -5,6 +5,7 @@ import { format } from "date-fns";
 import { Suspense } from "react";
 import IGLSimWrapper from "./igl-sim-wrapper";
 import { redirect, useRouter } from "next/navigation";
+import { m } from "framer-motion";
 
 async function IGLSimPage({
     searchParams,
@@ -15,29 +16,34 @@ async function IGLSimPage({
     const params = await searchParams
 
     const match_id = params[PARAM_NAMES.MATCH_ID] || undefined
+    const map = params[PARAM_NAMES.MAP] || DEFAULT_MAP_OPTION
+    const fromDate = params[PARAM_NAMES.FROM_DATE] || DEFAULT_DATE_RANGE.from
+    const toDate = params[PARAM_NAMES.TO_DATE] || DEFAULT_DATE_RANGE.to
+    const matchTypes = params[PARAM_NAMES.MATCH_TYPES]?.split(',') || DEFAULT_TYPE_OPTION
     let result: ActionReturn<FilteredMatchResponse> | undefined = undefined
+    const filterParams: FilteredMatchesParams = {
+        mapName: map,
+        matchTypes: matchTypes,
+        from: format(fromDate, "yyyy-MM-dd"),
+        to: format(toDate, "yyyy-MM-dd"),
+    }
+
     if (match_id) {
         result = await getMatchById(match_id)
     } else {
-        const map = params[PARAM_NAMES.MAP] || DEFAULT_MAP_OPTION
-        const fromDate = params[PARAM_NAMES.FROM_DATE] || DEFAULT_DATE_RANGE.from
-        const toDate = params[PARAM_NAMES.TO_DATE] || DEFAULT_DATE_RANGE.to
-        const matchTypes = params[PARAM_NAMES.MATCH_TYPES]?.split(',') || DEFAULT_TYPE_OPTION
-
-        const filterParams: FilteredMatchesParams = {
-            mapName: map,
-            matchTypes: matchTypes,
-            from: format(fromDate, "yyyy-MM-dd"),
-            to: format(toDate, "yyyy-MM-dd"),
-        }
-
         result = await getFilteredRandomMatch(filterParams)
-        const matchID = result.data?.matches.MatchID
-        if (matchID) {
-            redirect(`/dashboard/analytics/igl-simulator?${PARAM_NAMES.MATCH_ID}=${matchID}`)
-    }
-    }
 
+        const matchID = result.data?.matches.MatchID
+        let urlSearchParams = new URLSearchParams();
+        urlSearchParams.set(PARAM_NAMES.MAP, map);
+        urlSearchParams.set(PARAM_NAMES.FROM_DATE, format(fromDate, "yyyy-MM-dd"));
+        urlSearchParams.set(PARAM_NAMES.TO_DATE, format(toDate, "yyyy-MM-dd"));
+        urlSearchParams.set(PARAM_NAMES.MATCH_TYPES, matchTypes.join(","));
+        if (matchID) {
+            urlSearchParams.set(PARAM_NAMES.MATCH_ID, matchID);
+            redirect(`?${urlSearchParams.toString()}`)
+        }
+    }
 
     if (result.error) {
         return <div>{result.error}</div>
@@ -47,22 +53,9 @@ async function IGLSimPage({
         return <div>Match not found</div>
     }
 
-    const DEFAULT_FILTER: FilteredMatchesParams = {
-        mapName: DEFAULT_MAP_OPTION,
-        matchTypes: DEFAULT_TYPE_OPTION,
-        from: DEFAULT_DATE_RANGE.from.toString(),
-        to: DEFAULT_DATE_RANGE.to.toString(),
-        limit: DEFAULT_LIMIT,
-    }
-
-    let filter = DEFAULT_FILTER
-    if (result.data.filter) {
-        filter = result.data.filter
-    }
-
     return (
         <Suspense fallback={<div>Loading heatmap...</div>}>
-            <IGLSimWrapper filterParams={filter} match={result.data?.matches} />
+            <IGLSimWrapper filterParams={filterParams} match={result.data?.matches} />
         </Suspense>
     )
 }
